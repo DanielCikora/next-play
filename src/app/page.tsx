a00decaaf3684fdf9daf4637670add253c389458
Daniel Cikora <d.cikora98@gmail.com>
2025-05-04 22:56:44 +0200

    Games - Route Removed, now uses Default.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Games from "@/components/layout/games/Games";
+import Sidebar from "@/components/layout/sidebar/Sidebar";
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main className='games-page'>
+      <div className='wrapper flex md:flex-row flex-col md:justify-between md:items-start items-center'>
+        <Sidebar />
+        <Games />
+      </div>
+    </main>
+  );
 }
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,6 @@
 import { NavItemPropsDataType } from "@/types/props";
 export const navigationItems: NavItemPropsDataType[] = [
   { href: "/", linkText: "Home" },
-  { href: "/games", linkText: "Games" },
   { href: "/news", linkText: "News" },
   { href: "/about", linkText: "About" },
   { href: "/contact", linkText: "Contact" },
